ee32cc58932e1612c308f3fe98ef5df7ba3e2519
FlyingHail <flyinghail@msn.com>
2020-10-29 20:33:33 +0800

    get shell width in windows

--- src/Component/ProgressBar.php
+++ src/Component/ProgressBar.php
@@ -37,18 +37,32 @@ class ProgressBar
         $this->stream = $stream;
         $this->formatter = Formatter::getInstance();
 
-        if (\getenv('COLUMNS')) {
-            $this->terminalWidth = (int) \getenv('COLUMNS');
-        } else {
-            $paths = \explode(':', \getenv('PATH'));
-            foreach ($paths as $path) {
-                $bin = $path . DIRECTORY_SEPARATOR . 'tput';
-                if (\file_exists($bin) && \is_executable($bin)) {
-                    $this->terminalWidth = (int) \exec('tput cols');
-                    break;
-                }
+        if ($columns = \getenv('COLUMNS')) {
+            $this->terminalWidth = (int) $columns;
+        } elseif (PHP_OS_FAMILY === 'Windows') {
+            if (self::findCommand(';', 'powershell.exe')) {
+                $this->terminalWidth = (int) \exec('powershell $Host.UI.RawUI.WindowSize.Width');
+            } else {
+                $content = \shell_exec('mode');
+                \preg_match('/CON:(?:\n|.)*Columns:.*?(\d+)/', $content, $matches);
+                $this->terminalWidth = (int) $matches[1];
+            }
+        } elseif (self::findCommand(':', 'tput')) {
+            $this->terminalWidth = (int) \exec('tput cols');
+        }
+    }
+
+    private static function findCommand(string $split, string $file): bool
+    {
+        $paths = \explode($split, \getenv('PATH'));
+        foreach ($paths as $path) {
+            $bin = $path . DIRECTORY_SEPARATOR . $file;
+            if (\file_exists($bin) && \is_executable($bin)) {
+                return true;
             }
         }
+
+        return false;
     }
 
     public function setTitle(string $title): void
